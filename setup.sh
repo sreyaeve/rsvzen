922101ec67107e6deedc283de1d8d8b4deefb397
sreyaeve <121951124+sreyaeve@users.noreply.github.com>
2023-05-16 01:43:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -54,6 +54,23 @@ green='\e[0;32m'
 yell='\e[1;33m'
 tyblue='\e[1;36m'
 NC='\e[0m'
+# Color
+
+RED='\033[0;31m'
+
+NC='\033[0m'
+
+GREEN='\033[0;32m'
+
+ORANGE='\033[0;33m'
+
+BLUE='\033[0;34m'
+
+PURPLE='\033[0;35m'
+
+CYAN='\033[0;36m'
+
+LIGHT='\033[0;37m'
 purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
 tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
