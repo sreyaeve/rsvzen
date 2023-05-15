81cf0b0ef0521dc74406bded21aeb4d09edcc375
sreyaeve <121951124+sreyaeve@users.noreply.github.com>
2023-05-16 02:31:37 +0700

    Update m-trojan.sh

--- file/m-trojan.sh
+++ file/m-trojan.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipinfo.io/ip);
+
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 CEKEXPIRED () {
     today=$(date -d +1day +%Y-%m-%d)
